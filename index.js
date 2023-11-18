80547a5411f3bc6bc820773933bc7d68b6c06be6
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-11-18 15:41:33 -0600

    Done!

--- index.js
+++ index.js
@@ -61,11 +61,57 @@ const emp = [
         name: 'role'
     },
     {
-        type: 'input',
+        type: 'number',
         message: "Who's their manager?",
         name: 'manager'
     }
 ];
+
+const remdep = {
+    type: 'number',
+    message: 'Which department would you like to terminate?',
+    name: "id"
+};
+
+const remrol = {
+    type: 'number',
+    message: 'Which role would you like to terminate?',
+    name: "id"
+};
+
+const rememp = {
+    type: 'number',
+    message: 'Which employee would you like to terminate?',
+    name: "id"
+};
+
+const update = [
+    {
+        type: 'input',
+        message: "What is this employee's first name?",
+        name: 'firnam'
+    },
+    {
+        type: 'input',
+        message: "What is this employee's last name?",
+        name: 'lasnam'
+    },
+    {
+        type: 'number',
+        message: "What is this employee's role?",
+        name: 'rol'
+    },
+    {
+        type: 'number',
+        message: "What is this employee's manager?",
+        name: 'man'
+    },
+    {
+        type: 'input',
+        message: "What is this employee's id?",
+        name: 'id'
+    },
+]
 
 function output (responses){
     const db = require('./sever');
@@ -93,67 +139,91 @@ function output (responses){
             console.table(res);
             db.query(`INSERT INTO department (name) VALUES("${res.name}")`, 
         function (err, results){
-            console.log(results);
+            console.table(results);
+        });
+        db.query('SELECT * FROM tacker_db.department', function (err, results){
+            console.table(results);
         });
         })
     }
 
     else if (responses == "add a role"){
         inquirer.prompt(rol).then(res => {
-            console.log(res);
+            console.table(res);
             db.query(`INSERT INTO role (title, salary, department_id) 
             VALUES("${res.title}", ${res.salary}, ${res.depId})`, 
         function (err, results){
-            console.log(results);
+            console.table(results);
+        });
+        db.query('SELECT * FROM tacker_db.role', function (err, results){
+            console.table(results);
         });
         })
     }
 
     else if (responses == "add an employee"){
         inquirer.prompt(emp).then(res => {
-            console.log(res);
-            db.query(`INSERT INTO employess (first_name, last_name, role_id, manager_id) 
+            console.table(res);
+            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) 
             VALUES("${res.firnam}", "${res.lasnam}", ${res.role}, ${res.manager})`, 
         function (err, results){
-            console.log(results);
+            console.table(results);
+        });
+        db.query('SELECT * FROM tacker_db.employee', function (err, results){
+            console.table(results);
         });
         })
     }
 
     else if (responses == "remove a department"){
+        db.query('SELECT * FROM tacker_db.department', function (err, results){
+            console.table(results);
+        });
         inquirer.prompt(remdep).then(res => {
             console.table(res);
-            db.query(`DELETE FROM tacker_db.department WHERE id = ${res};")`, 
+            db.query(`DELETE FROM tacker_db.department WHERE id = ${res.id}`, 
         function (err, results){
-            console.log(results);
+            console.table(results);
         });
         })
     }
 
     else if (responses == "remove a role"){
+        db.query('SELECT * FROM tacker_db.role', function (err, results){
+            console.table(results);
+        });
         inquirer.prompt(remrol).then(res => {
-            console.log(res);
-            db.query(`DELETE FROM tacker_db.department WHERE id = ${res};`, 
+            console.table(res);
+        db.query(`DELETE FROM tacker_db.role WHERE id = ${res.id}`, 
         function (err, results){
-            console.log(results);
+            console.table(results);
         });
         })
     }
 
     else if (responses == "remove an employee"){
+        db.query('SELECT * FROM tacker_db.employee', function (err, results){
+            console.table(results);
+        });
         inquirer.prompt(rememp).then(res => {
-            console.log(res);
-            db.query(`DELETE FROM tacker_db.department WHERE id = ${res};)`, 
+            console.table(res);
+        db.query(`DELETE FROM tacker_db.employee WHERE id = ${res.id}`, 
         function (err, results){
-            console.log(results);
+            console.table(results);
         });
         })
     }
 
     else if (responses == "update an employee role"){
-        db.query('SELECT * FROM tacker_db.employee', function (err, results){
-            console.log(results);
-        });
+
+        inquirer.prompt(update).then(res => {
+            console.table(res);
+            db.query(`UPDATE employee 
+            SET first_name = '${res.firnam}', last_name = '${res.lasnam}', role_id = ${res.rol}, manager_id = ${res.man}
+            WHERE id = ${res.id}`, function (err, results){
+                console.table(results);
+            });
+        })
     }
 }
 
