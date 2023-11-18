db282b40466fa2241b0e458cef9d6386591a69a3
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-11-18 00:46:38 -0600

    crime

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,6 @@ CREATE TABLE employee (
     role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL
     manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
