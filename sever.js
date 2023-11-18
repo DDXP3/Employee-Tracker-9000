57a42293c83ce9b13b6548a63bbccb2b9b66be33
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-11-18 01:24:17 -0600

    treason

---
+++ sever.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//connect to data base
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'tacker_db'
+    },
+    console.log('Connected')
+);
+
+app.listen(PORT, () => {
+    console.log(`Hey, I'm listening to you from port ${PORT}`)
+});
+
+module.exports = db;
